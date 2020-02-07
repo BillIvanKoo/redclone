b6408f6414e3379ce6356def60b6e49c6ac1b72d
BillIvanKoo <kooslarto.bill.ivan@gmail.com>
2020-02-07 12:34:41 +0700

    can update and delete posts, create post get user from token now

--- server/src/main/java/com/redclone/server/controller/PostController.java
+++ server/src/main/java/com/redclone/server/controller/PostController.java
@@ -2,13 +2,18 @@ package com.redclone.server.controller;
 
 import com.redclone.server.exception.ResourceNotFoundException;
 import com.redclone.server.model.Post;
+import com.redclone.server.model.User;
 import com.redclone.server.repository.PostRepository;
 import com.redclone.server.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,12 +39,39 @@ public class PostController{
         return postRepository.findByUserId(userId, pageable);
     }
     
-    @PostMapping("/users/{userId}")
-    public Post postMethodName(@PathVariable(value = "userId") Long userId, @RequestBody Post post) {
-        return userRepository.findById(userId).map(user -> {
-            post.setUser(user);
-            return postRepository.save(post);
-        }).orElseThrow(() -> new ResourceNotFoundException("UserId " + userId + " not found"));
+    @PostMapping("/")
+    public Post createPost(@RequestBody Post post) {
+        User user = userRepository.findByUsername(getUsernameFromToken());
+        post.setUser(user);
+        return postRepository.save(post);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updatePost(@PathVariable(value="id") Long id, @RequestBody Post postRequest) {
+        return postRepository.findById(id).map(post -> {
+            if (!post.getUser().getUsername().equals(getUsernameFromToken())){
+                return ResponseEntity.badRequest().body("TOKEN USERNAME != TO POST USER");
+            }
+            post.setContent(postRequest.getContent());
+            postRepository.save(post);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + id + " not found"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable(value = "id") Long id){
+        return postRepository.findById(id).map(post -> {
+            if (!post.getUser().getUsername().equals(getUsernameFromToken())){
+                return ResponseEntity.badRequest().body("TOKEN USERNAME != TO POST USER");
+            }
+            postRepository.delete(post);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + id + " not found"));
     }
     
+    private String getUsernameFromToken(){
+        UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getUsername();
+    }
+
 }
--- server/src/main/java/com/redclone/server/exception/ResourceNotFoundException.java
+++ server/src/main/java/com/redclone/server/exception/ResourceNotFoundException.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+    
+    private static final long serialVersionUID = -1199414046025701480L;
+
     public ResourceNotFoundException() {
         super();
     }
